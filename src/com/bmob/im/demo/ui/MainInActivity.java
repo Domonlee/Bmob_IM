0957106a030426e0eb0d84572692847647615ce4
Domon <viplizhao@gmail.com>
2014-08-26 18:12:52 +0800

    v0.1.0

--- src/com/bmob/im/demo/ui/MainInActivity.java
+++ src/com/bmob/im/demo/ui/MainInActivity.java
@@ -1,12 +1,17 @@
 package com.bmob.im.demo.ui;
 
 import com.bmob.im.demo.R;
+import com.bmob.im.demo.ui.fragment.FindFragment;
+import com.bmob.im.demo.ui.fragment.MoneyFragment;
+import com.bmob.im.demo.ui.fragment.ShopFragment;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +25,10 @@ import android.widget.Toast;
 public class MainInActivity extends SlidingFragmentActivity {
 	private Button[] bTabs;
 	private TextView[] tTabs, lTabs;
+	private Fragment[] fragments;
+	private FindFragment findFragment;
+	private MoneyFragment moneyFragment;
+	private ShopFragment shopFragment;
 	private int bindex, tindex, lindex;
 	private int currentbTabIndex = 0;
 	private int currentlTabIndex = 0;
@@ -47,22 +56,24 @@ public class MainInActivity extends SlidingFragmentActivity {
 			}
 		});
 
-		bTabs[1].setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainInActivity.this,
-						FindActivity.class);
-				startActivity(intent);
-			}
-		});
 	}
 
 	private void initView() {
+		findFragment = new FindFragment();
+		moneyFragment = new MoneyFragment();
+		shopFragment = new ShopFragment();
+		fragments = new Fragment[] { shopFragment,moneyFragment,findFragment };
+		//hide 
+		getSupportFragmentManager().beginTransaction()
+				.add(R.id.fragment_container, findFragment)
+				.add(R.id.fragment_container, moneyFragment)
+				.add(R.id.fragment_container, shopFragment)
+				.hide(findFragment).hide(moneyFragment).show(shopFragment).commit();
+
 		bTabs = new Button[4];
 		bTabs[0] = (Button) findViewById(R.id.btn_bm_shop);
-		bTabs[1] = (Button) findViewById(R.id.btn_bm_find);
-		bTabs[2] = (Button) findViewById(R.id.btn_bm_money);
+		bTabs[1] = (Button) findViewById(R.id.btn_bm_money);
+		bTabs[2] = (Button) findViewById(R.id.btn_bm_find);
 		bTabs[3] = (Button) findViewById(R.id.btn_bm_group);
 
 		tTabs = new TextView[3];
@@ -81,7 +92,7 @@ public class MainInActivity extends SlidingFragmentActivity {
 		lTabs[0].setSelected(true);
 	}
 
-	private void onTabSelect(View v) {
+	public void onTabSelect(View v) {
 		switch (v.getId()) {
 		case R.id.btn_bm_shop:
 			bindex = 0;
@@ -92,9 +103,18 @@ public class MainInActivity extends SlidingFragmentActivity {
 		case R.id.btn_bm_find:
 			bindex = 2;
 			break;
-		case R.id.btn_bm_group:
-			bindex = 3;
-			break;
+//		case R.id.btn_bm_group:
+//			bindex = 3;
+//			break;
+		}
+		Toast.makeText(getApplicationContext(), bindex+ "", 1000).show();
+		if (currentbTabIndex != bindex) {
+			FragmentTransaction trx = getSupportFragmentManager().beginTransaction();
+			trx.hide(fragments[currentbTabIndex]);
+			if (!fragments[bindex].isAdded()) {
+				trx.add(R.id.fragment_container, fragments[bindex]);
+			}
+			trx.show(fragments[bindex]);
 		}
 		bTabs[currentbTabIndex].setSelected(false);
 		bTabs[bindex].setSelected(true);
---
+++ src/com/bmob/im/demo/ui/fragment/FindFragment.java
@@ -0,0 +1,32 @@
+package com.bmob.im.demo.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bmob.im.demo.R;
+import com.bmob.im.demo.ui.FragmentBase;
+
+public class FindFragment extends FragmentBase {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_find,null);
+	}
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+	}
+
+//		requestWindowFeature(Window.FEATURE_NO_TITLE);
+//		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//		setContentView(R.layout.activity_find);
+
+	
+}
